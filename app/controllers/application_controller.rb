1691f9806bd8e0e02038dd93954cb9f50ebd93d9
mateus-sartorio <mateussartorio39@gmail.com>
2025-08-03 16:37:31 -0300

    feat: disable CSRF across application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
   protect_from_forgery with: :null_session
+  skip_before_action :verify_authenticity_token
+  allow_browser versions: :modern
 end

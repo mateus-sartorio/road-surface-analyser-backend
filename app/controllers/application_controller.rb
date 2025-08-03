8cbfc77029dfff4f230267133fbd766851902861
mateus-sartorio <mateussartorio39@gmail.com>
2025-08-03 16:37:51 -0300

    feat: disable CSRF across application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
+  allow_browser versions: :modern
   protect_from_forgery with: :null_session
   skip_before_action :verify_authenticity_token
-  allow_browser versions: :modern
 end
